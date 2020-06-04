f88668e3a9b0736ccb7f17b1e06f5b43ad857288
Daithi Hearn <dahearn@deloitte.ie>
2020-06-04 23:35:14 +0200

    adding email pattern

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -339,16 +339,17 @@ class Home extends Component {
                             <tbody>
                               <tr><td>
                                 <Input
-                                className="currentEmail"
-                                id="currentEmail"
-                                type="input"
-                                name="currentEmail"
-                                placeholder="Email"
-                                autoComplete="off"
-                                onChange={this.handleChange}
-                                value={this.state.currentEmail}
-                                required
-                              /></td>
+                                  className="currentEmail"
+                                  id="currentEmail"
+                                  type="input"
+                                  pattern={"(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"}
+                                  name="currentEmail"
+                                  placeholder="Email"
+                                  autoComplete="off"
+                                  onChange={this.handleChange}
+                                  value={this.state.currentEmail}
+                                  required
+                                /></td>
                             <td>
                               <Button type="submit" color="link"><img alt="Add" src={AddIcon} width="20px" height="20px"/></Button>
                             {/* <button type="submit" color="danger"><img src={AddIcon} width="20px" height="20px"/></button> */}
